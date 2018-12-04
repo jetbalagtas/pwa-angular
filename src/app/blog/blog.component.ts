0fc412a1bd1179f35338175d25fa63d53616f096
Jet Balagtas <jetbalagtas@gmail.com>
2018-12-04 13:18:23 -0500

    setup

---
+++ src/app/blog/blog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.component.html',
+  styles: []
+})
+export class BlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
