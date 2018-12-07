d03f03618bd7e6bc7d9fafc21c404f9c93679303
Jet Balagtas <jetbalagtas@gmail.com>
2018-12-07 14:04:55 -0500

    cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { BlogComponent } from './blog/blog.component';
+import { UsersComponent } from './users/users.component';
+
+const routes: Routes = [
+  {path: '', component: BlogComponent},
+  {path: 'users', component: UsersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
